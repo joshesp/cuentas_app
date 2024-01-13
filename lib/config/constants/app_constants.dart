fa3bd5f6ea977c5945d39f4bb164ea9a107d9cee
Josh <josh@imac.com>
2024-01-13 16:40:21 -0600

    constantes para la aplicación en general

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,20 @@
+// Colors
+const colorPrimary = 0XFFDAEBD7;
+const colorPrimaryDark = 0XFF94BA8E;
+const colorAccent = 0XFFC2E7EA;
+const colorAccentLight = 0XFFEBF7F8;
+const colorDark = 0XFF191919;
+const colorLight = 0XFFF8FCF7;
+const colorGray = 0XFF5B5B5B;
+const colorGrayLight = 0XFFD8D8D8;
+const colorDanger = 0XFFF4BCBC;
+const colorDangerStrong = 0XFFFF6262;
+const colorDangerLight = 0XFFFFDCDC;
+
+// Margin and Padding
+const minSpacing = 16.0;
+const defaultSpacing = 24.0;
+const maxSpacing = 32.0;
+
+// Fonts
+const fontPrimary = 'Montserrat';
