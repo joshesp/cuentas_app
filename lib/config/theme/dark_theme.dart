0544b4840174b96ab074046a546ce8114449bd97
Josh <josh@imac.com>
2024-01-13 17:45:50 -0600

    definicion de textos  el los temas de la app

--- lib/config/theme/dark_theme.dart
+++ lib/config/theme/dark_theme.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cuentas_app/config/theme/coolors.dart';
 
+import 'text_style_theme.dart';
+
 ThemeData darkTheme() {
   final ThemeData base = ThemeData.dark();
 
   return base.copyWith(
-    primaryColor: Coolors.light,
+    primaryColor: Colors.white,
     scaffoldBackgroundColor: Coolors.dark,
+    textTheme: const TextTheme(
+      titleLarge: TextStyleTheme.titleLarge,
+      titleMedium: TextStyleTheme.titleMedium,
+      titleSmall: TextStyleTheme.titleSmall,
+      bodyLarge: TextStyleTheme.bodyLarge,
+      bodyMedium: TextStyleTheme.bodyMedium,
+      bodySmall: TextStyleTheme.bodySmall,
+    ),
   );
 }
--- lib/config/theme/light_theme.dart
+++ lib/config/theme/light_theme.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cuentas_app/config/theme/coolors.dart';
 
+import 'text_style_theme.dart';
+
 ThemeData lightTheme() {
   final ThemeData base = ThemeData.dark();
 
   return base.copyWith(
-    primaryColor: Coolors.primary,
+    primaryColor: Coolors.dark,
     scaffoldBackgroundColor: Colors.white,
+    textTheme: const TextTheme(
+      titleLarge: TextStyleTheme.titleLarge,
+      titleMedium: TextStyleTheme.titleMedium,
+      titleSmall: TextStyleTheme.titleSmall,
+      bodyLarge: TextStyleTheme.bodyLarge,
+      bodyMedium: TextStyleTheme.bodyMedium,
+      bodySmall: TextStyleTheme.bodySmall,
+    ),
   );
 }
