bef557bb81cb4c5039a4399cc846dd664ba40e89
Josh <josh@imac.com>
2024-01-27 17:41:42 -0600

    vista de login

--- lib/presentation/views/auth/sign_in/sign_in_view.dart
+++ lib/presentation/views/auth/sign_in/sign_in_view.dart
@@ -1,6 +1,9 @@
+import 'package:cuentas_app/common/widgets/input_custom/input_custom_widget_widget.dart';
 import 'package:cuentas_app/config/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+
+import '../../../../common/widgets/widgets.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
@@ -10,20 +13,59 @@ class SignInView extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(maxSpacing),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const Spacer(),
               SvgPicture.asset('assets/images/cuentas_app_iso.svg'),
+              const SizedBox(height: minSpacing),
               Text(
                 "Hola,\n¡Bienvenido!",
                 style: textTheme.titleLarge,
               ),
-              const TextField(
-                decoration: InputDecoration(
-                    hintText: 'Enter a search term',
-                    border: UnderlineInputBorder()),
+              const Spacer(),
+              const InputCustomWidget(
+                labelText: 'Correo',
+                hintText: 'Ingresa tu correo',
+                prefixIcon: Icons.alternate_email,
+              ),
+              const InputCustomWidget(
+                labelText: 'Contraseña',
+                hintText: 'Ingresa tu contraseña',
+                prefixIcon: Icons.lock,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 0, 12, 8),
+                    child: Text(
+                      'Olvidaste tu contraseña?',
+                      style: textTheme.bodyMedium!.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              ButtonAppWidget(
+                text: 'Ingresar',
+                icon: Icons.arrow_forward_ios,
+                fullWidth: true,
+                onPressed: () {},
+              ),
+              const SizedBox(height: minSpacing),
+              ButtonAppWidget(
+                text: 'Crear cuenta',
+                fullWidth: true,
+                styleType: ButtonStyleType.clear,
+                onPressed: () {},
               ),
             ],
           ),
