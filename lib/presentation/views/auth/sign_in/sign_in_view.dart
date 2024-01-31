29a6c67a224f982a6e3e3ab56e5247d7b0d28a6a
Josh <benjamin.espinosa.dev@gmail.com>
2024-01-30 21:08:25 -0600

    modal para recuperar la contraseña

--- lib/presentation/views/auth/sign_in/sign_in_view.dart
+++ lib/presentation/views/auth/sign_in/sign_in_view.dart
@@ -1,5 +1,6 @@
 import 'package:cuentas_app/common/widgets/input_custom/input_custom_widget_widget.dart';
 import 'package:cuentas_app/config/constants/app_constants.dart';
+import 'package:cuentas_app/config/theme/text_style_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -40,7 +41,9 @@ class SignInView extends StatelessWidget {
               Align(
                 alignment: Alignment.centerRight,
                 child: InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    _onShowModalBottomSheet(context);
+                  },
                   child: Padding(
                     padding: const EdgeInsets.fromLTRB(12, 0, 12, 8),
                     child: Text(
@@ -70,6 +73,48 @@ class SignInView extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  void _onShowModalBottomSheet(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    showModalBottomSheetCustom(
+      context: context,
+      contentWidgets: Column(
+        children: [
+          Text(
+            'Recuperar contraseña',
+            style: TextStyleTheme.subtitle.copyWith(
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: minSpacing),
+          Text(
+            'Ingresa tu correo y te enviaremos un link para que puedas restaurar tu contraseña.',
+            style: textTheme.bodyLarge,
+          ),
+          const SizedBox(height: maxSpacing),
+          const InputCustomWidget(
+            labelText: 'Correo',
+            hintText: 'Ingresa tu correo',
+            prefixIcon: Icons.alternate_email,
+          ),
+        ],
+      ),
+      buttonActions: [
+        ButtonAppWidget(
+          text: 'Recuperar contraseña',
+          fullWidth: true,
+          onPressed: () {},
+        ),
+        ButtonAppWidget(
+          text: 'Cancelar',
+          fullWidth: true,
+          styleType: ButtonStyleType.clear,
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
