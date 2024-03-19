a485aac55a6490878bd9c4b1f3b3e451f5669e7c
Josh <benjamin.espinosa.dev@gmail.com>
2024-03-18 21:12:37 -0600

    animacion intro de la aplicacion

--- lib/presentation/views/splashscreen/splascreen_view.dart
+++ lib/presentation/views/splashscreen/splascreen_view.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'dart:async';
 
 import 'package:cuentas_app/config/constants/app_constants.dart';
 import 'package:cuentas_app/config/theme/coolors.dart';
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class SplashscreenView extends StatelessWidget {
   const SplashscreenView({super.key});
@@ -15,36 +16,79 @@ class SplashscreenView extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(defaultSpacing),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Spacer(),
-              // TODO: Add Rive component
-              Center(
+              const Center(
                 child: SizedBox(
-                  child: SvgPicture.asset(
-                    'assets/images/cuentas_app_logotipo.svg',
+                  width: 160,
+                  height: 160,
+                  child: RiveAnimation.asset(
+                    'assets/animations/logo-intro.riv',
                   ),
                 ),
               ),
               const Spacer(),
-              SizedBox(
-                width: 24,
-                height: 24,
-                child: CircularProgressIndicator(
-                  backgroundColor: Coolors.light.withOpacity(0.5),
-                  color: Coolors.primary,
-                  strokeWidth: 2,
+              FadeAnimationWidget(
+                duration: const Duration(milliseconds: 800),
+                delay: const Duration(seconds: 2),
+                fadeOutAction: false,
+                child: Text(
+                  'Toma el control de tu contabilidad',
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Coolors.primary,
+                      ),
                 ),
-              ),
-              const SizedBox(height: 14),
-              Text(
-                'Toma el control de tu contabilidad',
-                style: Theme.of(context).textTheme.bodySmall,
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class FadeAnimationWidget extends StatefulWidget {
+  final Widget child;
+  final bool fadeOutAction;
+  final Duration duration;
+  final Duration delay;
+
+  const FadeAnimationWidget({
+    super.key,
+    required this.child,
+    required this.duration,
+    this.fadeOutAction = true,
+    this.delay = const Duration(milliseconds: 0),
+  });
+
+  @override
+  State<FadeAnimationWidget> createState() => _FadeAnimationWidgetState();
+}
+
+class _FadeAnimationWidgetState extends State<FadeAnimationWidget> {
+  bool _visible = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _visible = widget.fadeOutAction;
+    });
+
+    Timer(widget.delay, () {
+      setState(() {
+        _visible = !_visible;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      opacity: _visible ? 1.0 : 0.0,
+      duration: widget.duration,
+      child: widget.child,
     );
   }
 }
