5039f6dc2a0f2df8e3be68e1d2b05df3890a0583
Josh <benjamin.espinosa.dev@gmail.com>
2024-03-20 20:36:42 -0600

    splash redirect

--- lib/presentation/views/splashscreen/splascreen_view.dart
+++ lib/presentation/views/splashscreen/splascreen_view.dart
@@ -5,15 +5,32 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rive/rive.dart';
 
-class SplashscreenView extends StatelessWidget {
+class SplashscreenView extends StatefulWidget {
   static const name = "splashScreen";
 
   const SplashscreenView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    redirectTo(context);
+  State<SplashscreenView> createState() => _SplashscreenViewState();
+}
 
+class _SplashscreenViewState extends State<SplashscreenView> {
+  late StateMachineController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeEventListener(onRiveEvent);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Coolors.dark,
       body: SafeArea(
@@ -22,12 +39,13 @@ class SplashscreenView extends StatelessWidget {
           child: Column(
             children: [
               const Spacer(),
-              const Center(
+              Center(
                 child: SizedBox(
                   width: 160,
                   height: 160,
                   child: RiveAnimation.asset(
-                    'assets/animations/logo-intro.riv',
+                    'assets/animations/logo_splash_intro.riv',
+                    onInit: onInit,
                   ),
                 ),
               ),
@@ -49,7 +67,23 @@ class SplashscreenView extends StatelessWidget {
     );
   }
 
-  void redirectTo(BuildContext context) {
+  void onInit(Artboard artboard) async {
+    _controller = StateMachineController.fromArtboard(
+      artboard,
+      'State Machine 1',
+    )!;
+    artboard.addController(_controller);
+
+    _controller.addEventListener(onRiveEvent);
+  }
+
+  void onRiveEvent(RiveEvent event) {
+    if (event.name == 'initAnimation') {
+      redirectTo();
+    }
+  }
+
+  void redirectTo() {
     Future.delayed(const Duration(seconds: 2), () {
       context.go('/intro');
     });
