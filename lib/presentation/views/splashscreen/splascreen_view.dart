070963656bc3a6c1d886b89073b518ed12e5bca9
Josh <josh@imac.com>
2024-03-23 14:26:30 -0600

    actualización de animaciones rive y splashscreen

--- lib/presentation/views/splashscreen/splascreen_view.dart
+++ lib/presentation/views/splashscreen/splascreen_view.dart
@@ -1,90 +1,36 @@
-import 'package:cuentas_app/common/utils/fade_animation_util.dart';
-import 'package:cuentas_app/config/constants/app_constants.dart';
 import 'package:cuentas_app/config/theme/coolors.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rive/rive.dart';
 
-class SplashscreenView extends StatefulWidget {
+class SplashscreenView extends StatelessWidget {
   static const name = "splashScreen";
 
   const SplashscreenView({super.key});
 
   @override
-  State<SplashscreenView> createState() => _SplashscreenViewState();
-}
-
-class _SplashscreenViewState extends State<SplashscreenView> {
-  late StateMachineController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.removeEventListener(onRiveEvent);
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    redirectTo(context);
+
+    return const Scaffold(
       backgroundColor: Coolors.dark,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(defaultSpacing),
-          child: Column(
-            children: [
-              const Spacer(),
-              Center(
-                child: SizedBox(
-                  width: 160,
-                  height: 160,
-                  child: RiveAnimation.asset(
-                    'assets/animations/logo_splash_intro.riv',
-                    onInit: onInit,
-                  ),
-                ),
-              ),
-              const Spacer(),
-              FadeAnimationUtil(
-                duration: const Duration(milliseconds: 800),
-                delay: const Duration(seconds: 2),
-                child: Text(
-                  'Toma el control de tu contabilidad',
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Coolors.primary,
-                      ),
-                ),
-              ),
-            ],
+        child: Center(
+          child: SizedBox(
+            width: 160,
+            height: 120,
+            child: RiveAnimation.asset(
+              'assets/animations/logo_splash_intro.riv',
+              fit: BoxFit.contain,
+            ),
           ),
         ),
       ),
     );
   }
 
-  void onInit(Artboard artboard) async {
-    _controller = StateMachineController.fromArtboard(
-      artboard,
-      'State Machine 1',
-    )!;
-    artboard.addController(_controller);
-
-    _controller.addEventListener(onRiveEvent);
-  }
-
-  void onRiveEvent(RiveEvent event) {
-    if (event.name == 'initAnimation') {
-      redirectTo();
-    }
-  }
-
-  void redirectTo() {
-    Future.delayed(const Duration(milliseconds: 4900), () {
+  void redirectTo(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 2500), () {
       context.go('/intro');
     });
   }
