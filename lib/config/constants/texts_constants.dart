87aaf29387b025ad6a9a5fcfd6a77671dbf9b473
Josh <josh@imac.com>
2024-03-23 13:43:47 -0600

    actualización de textos

--- lib/config/constants/texts_constants.dart
+++ lib/config/constants/texts_constants.dart
@@ -3,19 +3,19 @@ import 'package:cuentas_app/common/widgets/slider/model/slide_model.dart';
 
 List<SlideModel> sliderIntro = [
   SlideModel(
-    image: "assets/images/slide-1.png",
+    image: "assets/images/slide-3.png",
     title: "Captura tu movimento",
-    description: "Agrega tus ingreso y gastos de manera rápida y fácil.",
+    description: "Agrega tus ingresos y gastos de manera rápida y fácil.",
   ),
   SlideModel(
     image: "assets/images/slide-2.png",
     title: "Balance",
-    description: "Observa y analiza el balance de tus ingresos diarios.",
+    description: "Observa y analiza el balance de tus movimientos diarios.",
   ),
   SlideModel(
-    image: "assets/images/slide-3.png",
+    image: "assets/images/slide-1.png",
     title: "Cálculo de inpuestos",
-    description: "Obtén tu reporte aproximado de tus impuestos mensuales.",
+    description: "Obtén el cálculo aproximado de tus impuestos del mes.",
   ),
 ];
 
