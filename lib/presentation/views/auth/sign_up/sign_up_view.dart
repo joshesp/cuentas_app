2127e8c91949ca8cdd214133bd6fad9f24907684
Josh <josh@imac.com>
2024-03-26 17:04:20 -0600

    corrección de scroll en la visat de registro

--- lib/presentation/views/auth/sign_up/sign_up_view.dart
+++ lib/presentation/views/auth/sign_up/sign_up_view.dart
@@ -1,3 +1,4 @@
+import 'package:cuentas_app/common/utils/index.dart';
 import 'package:cuentas_app/common/widgets/button_app/button_app_widget.dart';
 import 'package:cuentas_app/common/widgets/button_app/text_link_widget.dart';
 import 'package:cuentas_app/common/widgets/input_custom/input_custom_widget_widget.dart';
@@ -14,63 +15,98 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeScreen = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crear cuenta'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(maxSpacing),
-          child: Column(
-            children: [
-              const InputCustomWidget(
-                labelText: 'Nombre',
-                hintText: 'Ingresa tu nombre',
-                prefixIcon: Icons.person,
-              ),
-              const InputCustomWidget(
-                labelText: 'Correo',
-                hintText: 'Ingresa tu correo',
-                prefixIcon: Icons.alternate_email,
-              ),
-              const InputCustomWidget(
-                labelText: 'Contraseña',
-                hintText: 'Ingresa tu contraseña',
-                prefixIcon: Icons.lock,
-              ),
-              const InputCustomWidget(
-                labelText: 'Confirmar contraseña',
-                hintText: 'Ingresa tu contraseña',
-                prefixIcon: Icons.lock,
-                suffixIcon: Icons.visibility,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: GestureDetector(
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          behavior: HitTestBehavior.opaque,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            reverse: true,
+            child: Container(
+              height: sizeScreen.height - (maxSpacing * 2),
+              padding: const EdgeInsets.all(maxSpacing),
+              child: const Column(
                 children: [
-                  Checkbox(
-                    value: false,
-                    onChanged: (value) {},
-                  ),
-                  Expanded(
-                    child: _createAccountWidget(
-                        context, Theme.of(context).textTheme),
-                  ),
+                  FormSignUpWidget(),
+                  SizedBox(height: 24),
+                  AuthBySocialNetworkWidget(),
                 ],
               ),
-              const SizedBox(height: defaultSpacing),
-              ButtonAppWidget(
-                text: 'Crear cuenta',
-                icon: Icons.arrow_forward_ios,
-                fullWidth: true,
-                onPressed: () {},
-              ),
-              const SizedBox(height: 24),
-              const AuthBySocialNetworkWidget(),
-            ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class FormSignUpWidget extends StatefulWidget {
+  const FormSignUpWidget({super.key});
+
+  @override
+  State<FormSignUpWidget> createState() => _FormSignUpWidgetState();
+}
+
+class _FormSignUpWidgetState extends State<FormSignUpWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const InputCustomWidget(
+          labelText: 'Nombre',
+          hintText: 'Ingresa tu nombre',
+          prefixIcon: Icons.person,
+        ),
+        const InputCustomWidget(
+          labelText: 'Correo electrónico',
+          hintText: 'Ingresa tu correo electrónico',
+          prefixIcon: Icons.alternate_email,
+          keyboardType: TextInputType.emailAddress,
+          validator: emailValidator,
+        ),
+        const SizedBox(height: 8),
+        const InputCustomWidget(
+          labelText: 'Contraseña',
+          hintText: 'Ingresa tu contraseña',
+          prefixIcon: Icons.lock,
+          obscureText: true,
+          validator: passwordValidator,
+        ),
+        const InputCustomWidget(
+          labelText: 'Confirmar contraseña',
+          hintText: 'Ingresa tu contraseña',
+          prefixIcon: Icons.lock,
+          suffixIcon: Icons.visibility,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Checkbox(
+              value: false,
+              onChanged: (value) {},
+            ),
+            Expanded(
+              child: _createAccountWidget(context, Theme.of(context).textTheme),
+            ),
+          ],
+        ),
+        const SizedBox(height: defaultSpacing),
+        ButtonAppWidget(
+          text: 'Crear cuenta',
+          icon: Icons.arrow_forward_ios,
+          fullWidth: true,
+          onPressed: () {},
+        ),
+      ],
     );
   }
 
